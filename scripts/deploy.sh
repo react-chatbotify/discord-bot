9a04c00a36c25aa0d334acba6c2c90599ab7a12c
tjtanjin <cjtanjin@gmail.com>
2025-03-30 17:08:58 +0800

    fix: Fix deployment logic

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,15 +7,6 @@ if [ -z "$APPLICATION_IMAGE" ]; then
   exit 1
 fi
 
-# Checks if deployment environment is specified
-if [ -z "$DEPLOY_ENV" ]; then
-  echo "[ERROR] DEPLOY_ENV variable not set. Please set it to 'dev' or 'prod'."
-  exit 1
-fi
-
-# Defines path to deployment files
-COMPOSE_PATH="/opt/rcb-deployments/discord-bot"
-
 # Logs into GHCR using provided credentials (from GitHub CI/CD)
 echo "Logging into GitHub Container Registry..."
 echo "${GHCR_PAT}" | docker login ghcr.io -u "${GHCR_USER}" --password-stdin
@@ -24,34 +15,20 @@ echo "${GHCR_PAT}" | docker login ghcr.io -u "${GHCR_USER}" --password-stdin
 echo "Pulling image: $APPLICATION_IMAGE"
 docker pull "$APPLICATION_IMAGE"
 
-# Prepares env and compose files based on environment
-if [ "$DEPLOY_ENV" == "production" ]; then
-  OVERRIDE_FILE="$COMPOSE_PATH/docker-compose.prod.yml"
-  COMPOSE_FILES="-f docker-compose.yml -f docker-compose.prod.yml"
-  ENV_FILE="--env-file .env.prod"
-elif [ "$DEPLOY_ENV" == "development" ]; then
-  OVERRIDE_FILE="$COMPOSE_PATH/docker-compose.dev.yml"
-  COMPOSE_FILES="-f docker-compose.yml -f docker-compose.dev.yml"
-  ENV_FILE="--env-file .env.development"
-else
-  echo "[ERROR] Unknown DEPLOY_ENV: $DEPLOY_ENV. Expected 'development' or 'production'."
-  exit 1
-fi
+# Changes directory to where the deployment files are
+cd "/opt/rcb-deployments/$PROJECT_NAME"
 
 # Replaces placeholder string '${APPLICATION_IMAGE}' with the actual image within compose file.
-echo "Injecting image into override file ($OVERRIDE_FILE)..."
-sed -i "s|\${APPLICATION_IMAGE}|$APPLICATION_IMAGE|g" "$OVERRIDE_FILE"
-
-# Changes directory to where the deployment files are
-cd "$COMPOSE_PATH"
+echo "Injecting image to override docker compose file..."
+sed -i "s|\${APPLICATION_IMAGE}|$APPLICATION_IMAGE|g" ./docker-compose.override.yml
 
 # Tears down existing containers
-echo "Stopping existing containers for environment: $DEPLOY_ENV..."
-docker compose -p "$PROJECT_NAME" $COMPOSE_FILES down
+echo "Stopping existing containers..."
+docker compose -p "$PROJECT_NAME" down
 
 # Brings up new containers
-echo "Starting docker-compose for environment: $DEPLOY_ENV..."
-docker compose -p "$PROJECT_NAME" $ENV_FILE $COMPOSE_FILES up -d --build
+echo "Starting new containers..."
+docker compose -p "$PROJECT_NAME" up -d --build
 
 # Cleans up unused docker images
 echo "Pruning unused Docker images..."
